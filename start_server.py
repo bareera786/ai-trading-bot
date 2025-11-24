1e4a627c1b8b82d0978fd250d13a539c29722d1d
Muhammad Tahir Akram <tahir@192.168.1.14>
2025-11-24 11:37:31 +0500

    Update server port from 5001 to 5000 in start_server.py

--- start_server.py
+++ start_server.py
@@ -1,4 +1,4 @@
 import sys
 sys.path.insert(0, '.')
 from ai_ml_auto_bot_final import app
-app.run(host='0.0.0.0', port=5001, debug=False, threaded=True)
+app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
